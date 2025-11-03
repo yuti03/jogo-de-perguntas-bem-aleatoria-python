ff304fa3146518053c8065e4bc3c7b4c65526529
yuti03 <yuri.nunes.silva@escola.pr.gov.br>
2025-11-03 09:52:30 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,1 +1,28 @@
+print ('bom dia,boa tarde e boa noite')
+acertou = 0
 
+#descrução
+print("jogo das palavras")
+#perguntas
+print('como vc se chama?')
+chama = input('escreva seu nome')
+
+if(chama == "yuti"):
+       print('senha correta mas ainda não tenho certesa vou ter que fazer outras perguntas')
+       acertou +=1
+else:
+       print('senha incorreta tente novamente')
+       
+       input('como vc se chama')
+
+if(acertou == 1):
+       print('mikel jackson está vivo e mora em chique chique bahia')
+       
+       input('isso é verdade ou mentira:')
+       
+       if(input=='mentira'):
+          print('acertou mais ainda tenho que perguntar outra coisa')
+
+    
+    
+#jogo de perguntas
